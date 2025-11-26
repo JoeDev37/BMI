5f939b1fd0552e5abdbc24d5d115930dab26132c
JoeDev37 <alonejoe367@gmail.com>
2025-11-26 11:51:58 +0300

    Body Mass Index(coding)

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 
-console.log('Hello, World!!');
+// console.log('Hello, World!!');
+
+const convBtn = document.getElementById('convBtn');
+const hInput = document.getElementById('hInput');
+const wInput = document.getElementById('wInput');
+const result = document.getElementById('result');
+const commnet = document.getElementById('commnet');
+
+convBtn.addEventListener('click', () => {
+
+    let h = Number(hInput.value);
+    let w = Number(wInput.value);
+
+    if (isNaN(h) || isNaN(w) || h <= 0 || w <= 0) {
+        alert('Please enter valid numbers for both height and weight!');
+        return;
+    }
+
+    console.log("h: " + h + ", w: " + w)
+    
+})
+
+
