b79e0cdc6e4af7c9f854e59740420e7624803112
JoeDev37 <alonejoe367@gmail.com>
2025-11-27 19:57:41 +0300

    Body Mass Index(BMI)

--- main.js
+++ main.js
@@ -10,14 +10,33 @@ const commnet = document.getElementById('commnet');
 convBtn.addEventListener('click', () => {
 
     let h = Number(hInput.value);
+        h = h/100;
     let w = Number(wInput.value);
+
+    const bmi = w/(h*h);
 
     if (isNaN(h) || isNaN(w) || h <= 0 || w <= 0) {
         alert('Please enter valid numbers for both height and weight!');
         return;
     }
 
-    console.log("h: " + h + ", w: " + w)
+
+    if (bmi < 18.5) {
+        commnet.textContent = 'Under Weight!!';
+        commnet.style.color = '#f38ba8';
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        commnet.textContent = 'Normal Weight';
+        commnet.style.color = '#a6e3a1'; 
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        commnet.textContent = 'Over Weight!!';
+        commnet.style.color = '#f38ba8';
+    } else if (bmi > 30) {
+        commnet.textContent = 'Obesity!!';
+        commnet.style.color = '#f38ba8';
+    }
+
+
+    result.value = bmi.toFixed(2)
     
 })
 
